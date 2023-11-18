ad025c8511a71f6ad9cb416512cc701fa0104980
dszopinski <damian.szopinski@verestro.com>
2023-11-18 13:45:35 +0100

    Create handlers for buttons.

--- src/gui/window/imp.rs
+++ src/gui/window/imp.rs
@@ -1,7 +1,7 @@
 use adw::glib::subclass::InitializingObject;
 use adw::subclass::prelude::ObjectSubclass;
 use adw::subclass::prelude::*;
-use gtk::{glib, CompositeTemplate};
+use gtk::{glib, CompositeTemplate, template_callbacks, Button};
 
 #[derive(CompositeTemplate, Default)]
 #[template(resource = "/org/shiishiji/integration2/window.ui")]
@@ -15,7 +15,7 @@ impl ObjectSubclass for Window {
 
     fn class_init(klass: &mut Self::Class) {
         klass.bind_template();
-        // klass.bind_template_callbacks();
+        klass.bind_template_callbacks();
     }
 
     fn instance_init(obj: &InitializingObject<Self>) {
@@ -23,7 +23,25 @@ impl ObjectSubclass for Window {
     }
 }
 
-impl Window {}
+#[template_callbacks]
+impl Window {
+
+    #[template_callback]
+    pub async fn handle_nr_of_laptops_by_manufacturer(&self, _: Button) {
+        println!("handle handle_nr_of_laptops_by_manufacturer");
+    }
+
+    #[template_callback]
+    pub async fn handle_nr_of_laptops_by_screen_proportions(&self, _: Button) {
+        println!("handle handle_nr_of_laptops_by_screen_proportions");
+    }
+
+    #[template_callback]
+    pub async fn handle_export_laptops(&self, _: Button) {
+        println!("handle handle_export_laptops");
+    }
+
+}
 
 impl ObjectImpl for Window {
     fn constructed(&self) {
